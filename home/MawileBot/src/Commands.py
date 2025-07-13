db28a1e6b68ae5a93c4a9536f51b7a3b5f63ef15
Daddodad <dav.moretti2@stud.uniroma3.it>
2025-07-13 14:45:54 +0200

    card

--- home/MawileBot/src/Commands.py
+++ home/MawileBot/src/Commands.py
@@ -1445,19 +1445,26 @@ def get_fight_conversation_handler():
 
 # --------------------------------------------------------------------------- CARD -------------------------------------------------------------------------------------------
 def get_pokemon_image_path(pokemon_name):
+
     """
     Returns the path for the Pokemon image based on the Pokemon name.
-    Searches for the image in the /images/pokemons folder (case-insensitive).
+    Searches for the image in the /images/pokemons folder.
     Returns 'Missing.png' if the specific Pokemon image is not found.
     """
-    base_path = os.path.join(ENV_PATH, "images", "pokemons")
+    base_path = ENV_PATH+"/images/pokemons"
+    pokemon_image = f"{pokemon_name}.png"
+    full_path = os.path.join(base_path, pokemon_image)
 
     try:
         for file in os.listdir(base_path):
             if file.lower() == f"{pokemon_name.lower()}.png":
-                return os.path.join(base_path, file)
-    except Exception as e:  
-        print(f"Errore nella ricerca dell'immagine per card: {e}")
+                full_path = os.path.join(base_path, file)
+    except Exception as e:
+        print(f"card error: {e}")
+
+    if os.path.exists(full_path):
+        return full_path
+    else:
         return os.path.join(base_path, "Missing.png")
 
 async def card_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
