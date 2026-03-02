1773cf96d2159ce04a8a0d74834b562726ea1bd8
Daddodad <dav.moretti2@stud.uniroma3.it>
2026-03-02 10:43:59 +0100

    FIX: Utility change

--- home/MawileBot/src/BeatlesBoy_utils.py
+++ home/MawileBot/src/BeatlesBoy_utils.py
@@ -75,9 +75,10 @@ async def pokemon_utility(pokemon,lvl):
     pokemon = await similar_pokemon_name(pokemon.lower())
     max_bst = await get_poke_bst(await find_evo_at_level_x(pokemon, 100))
 
-    utility = max_bst/620*10
+    utility_bst = max_bst/620*10
+    utility_lvl = lvl/10
 
-
+    utility = utility_bst * 0.5 + utility_lvl * 0.5
     return round(utility, 2)  # 0-10 scale
 
 async def filter_team(team, remove_100=False):
