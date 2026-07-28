aabece051146134d6ecc4ab79680958ff64b5f41
Daddodad <davide.daddo@gmail.com>
2026-07-28 19:52:30 +0200

    FIX: Lega (bonus)

--- home/MawileBot/src/poke_lib.py
+++ home/MawileBot/src/poke_lib.py
@@ -1512,7 +1512,7 @@ def create_pokemon_collage(df, type = 'gym', path=None, enemy_powers=None):
                         bg = (99, 238, 99)
                     elif int(bonus) < 0:
                         bg = (255, 111, 111)               
-                    text_image = create_text_image(df[column_name][index], bg,(False,False,False,False))  # White text on blue
+                    text_image = create_text_image(bonus, bg,(False,False,False,False))  # White text on blue
                 collage_image.paste(text_image, position)
 
     # Save or return the final collage image
